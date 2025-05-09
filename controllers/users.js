b4309d709a46598231f9c8fdb2412b7b2610e493
erradical64 <asa@nbenson.com>
2025-05-09 12:03:47 -0600

    Completed Week 1 Progress (Hopefully)

---
+++ controllers/users.js
@@ -0,0 +1,24 @@
+const mongodb = require('../data/database');
+const ObjectID = require('mongodb').ObjectID;
+
+const getAll = async (req, res) => {
+    const result = await mongodb.getDatabase().db().collection('users').find();
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users);
+    });
+}
+
+const getSingle = async (req, res) => {
+    const userId = new ObjectID(re.params.id);
+    const result = await mongodb.getDatabase().db().collection('users').find({_id: userID});
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users[0]);
+    });
+};
+
+module.exports = {
+    getAll,
+    getSingle
+}
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,6 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {res.send('今日は、世界！');});
 
+router.use('/users', require('./users'));
+
 module.exports = router;
