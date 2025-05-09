8081943cf6fbaa4265969257cda48f4f4d7a95c0
erradical64 <asa@nbenson.com>
2025-05-09 16:03:53 -0600

    フィクス！

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {res.send('今日は、世界！');});
+router.get('/', (req, res) => {res.send('こんにちは、世界！');});
 
 router.use('/users', require('./users'));
 
